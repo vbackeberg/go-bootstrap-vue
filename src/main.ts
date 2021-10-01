f49f43f14b94c0b75e8164bca2612ec7d14e044e
Valerian Backeberg <valerian@backebergs.de>
2021-10-01 23:20:11 +0200

    router

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHashHistory} from 'vue-router'
+import Assignments from '@/components/Assignments.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/assignments', component: Assignments },
+  { path: '/:pathMatch(.*)*', redirect: '/assignments' },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+createApp(App).use(router).mount('#app')
