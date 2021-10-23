3570a3259a5f04876dd5ab8da38b655cc43c1517
Valerian Backeberg <valerian@backebergs.de>
2021-10-23 19:25:32 +0200

    Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,28 +1,29 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <router-link class="navbar-brand" to="/assignments">
+        <img src="@/assets/brand.svg" height="38" width="38" />
+      </router-link>
       <button
         class="navbar-toggler"
         type="button"
         data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+        data-bs-target="#navbarTop"
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="collapse navbar-collapse" id="navbarTop">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#">Assignments</a>
+            <router-link class="nav-link" to="/assignments"
+              >Assignments</router-link
+            >
           </li>
           <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#">Boids</a>
+            <router-link class="nav-link" to="/boids">Boids</router-link>
           </li>
         </ul>
-        <button class="btn btn-secondary">
+        <button class="btn btn-primary">
           <i class="bi bi-person-fill"></i>
         </button>
       </div>
