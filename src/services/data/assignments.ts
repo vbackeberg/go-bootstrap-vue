e5e3751849d885f59d1b63c6d7ff1194b2cb6965
Valerian Backeberg <valerian@backebergs.de>
2021-10-23 19:25:08 +0200

    mocked data: assignments and boids

---
+++ src/services/data/assignments.ts
@@ -0,0 +1,137 @@
+export interface Assignment {
+    id: number;
+    name: string;
+    description: string;
+  }
+  
+  const exampleDescription =
+    "Some quick example text to build on the card title and make up the bulk of the card's content.";
+  
+  export const returnedAssignments: Assignment[] = [
+    {
+      description: exampleDescription,
+      id: 1,
+      name: "CHOAM",
+    },
+    {
+      description: exampleDescription,
+      id: 2,
+      name: "Acme Corp",
+    },
+    {
+      description: exampleDescription,
+      id: 3,
+      name: "Sirius Cybernetics Corp",
+    },
+    {
+      description: exampleDescription,
+      id: 4,
+      name: "MomCorp",
+    },
+    {
+      description: exampleDescription,
+      id: 5,
+      name: "Rich Industries",
+    },
+    {
+      description: exampleDescription,
+      id: 6,
+      name: "Soylent Corp",
+    },
+    {
+      description: exampleDescription,
+      id: 7,
+      name: "Very Big Corp",
+    },
+    {
+      description: exampleDescription,
+      id: 8,
+      name: "Frobozz Magic Co",
+    },
+    {
+      description: exampleDescription,
+      id: 9,
+      name: "Warbucks Industries",
+    },
+    {
+      description: exampleDescription,
+      id: 10,
+      name: "Tyrell Corp",
+    },
+    {
+      description: exampleDescription,
+      id: 11,
+      name: "Wayne Enterprises",
+    },
+    {
+      description: exampleDescription,
+      id: 12,
+      name: "Virtucon",
+    },
+    {
+      description: exampleDescription,
+      id: 13,
+      name: "Globex",
+    },
+    {
+      description: exampleDescription,
+      id: 14,
+      name: "Umbrella Corp",
+    },
+    {
+      description: exampleDescription,
+      id: 15,
+      name: "Wonka Industries",
+    },
+    {
+      description: exampleDescription,
+      id: 16,
+      name: "Stark Industries",
+    },
+    {
+      description: exampleDescription,
+      id: 17,
+      name: "Clampett Oil",
+    },
+    {
+      description: exampleDescription,
+      id: 18,
+      name: "Oceanic Airlines",
+    },
+    {
+      description: exampleDescription,
+      id: 19,
+      name: "Yoyodyne Propulsion Sys",
+    },
+    {
+      description: exampleDescription,
+      id: 20,
+      name: "Cyberdyne Systems Corp",
+    },
+    {
+      description: exampleDescription,
+      id: 21,
+      name: "d'Anconia Copper",
+    },
+    {
+      description: exampleDescription,
+      id: 22,
+      name: "Gringotts",
+    },
+    {
+      description: exampleDescription,
+      id: 23,
+      name: "Oscorp",
+    },
+    {
+      description: exampleDescription,
+      id: 24,
+      name: "Nakatomi Trading Corp",
+    },
+    {
+      description: exampleDescription,
+      id: 25,
+      name: "Spacely Space Sprockets",
+    },
+  ];
+  
