dfffd68af52313837a2569964d30f1476599d1ec
Valerian Backeberg <valerian@backebergs.de>
2021-10-23 19:26:05 +0200

    Define routes. Use webhistory

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import {createRouter, createWebHashHistory} from 'vue-router'
-import Assignments from '@/components/Assignments.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHashHistory, createWebHistory } from "vue-router";
+import Assignments from "@/components/Assignments.vue";
+import Boids from "@/components/Boids.vue";
 
 const routes = [
-  { path: '/assignments', component: Assignments },
-  { path: '/:pathMatch(.*)*', redirect: '/assignments' },
-]
+  { path: "/", component: Assignments },
+  { path: "/assignments", component: Assignments },
+  { path: "/boids", component: Boids }
+];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-})
+  history: createWebHistory(),
+  routes
+});
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .mount("#app");
