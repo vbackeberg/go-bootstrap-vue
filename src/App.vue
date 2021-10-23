2231514311210ccaa34c6985e88866e7f431025f
Valerian Backeberg <valerian@backebergs.de>
2021-10-23 19:45:13 +0200

    getOrCreate all modals when updated.

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,19 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import Navbar from "@/components/Navbar.vue";
+import { Modal } from "bootstrap";
 
 export default defineComponent({
   name: "App",
-  components: { Navbar }
+  components: { Navbar },
+
+  updated() {
+    document
+      .querySelectorAll('[data-bs-toggle="modal"]')
+      .forEach(modalTrigger => {
+        Modal.getOrCreateInstance(modalTrigger);
+      });
+  }
 });
 </script>
 
