75844cc76d64c10116268d4e7f4648e8096977c0
Valerian Backeberg <valerian@backebergs.de>
2021-11-14 11:52:46 +0100

    reset state

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <navbar></navbar>
-  <main class="mt-4">
+  <main>
     <router-view></router-view>
   </main>
 </template>
@@ -24,14 +24,4 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss">
-$secondary: #4d4d4d;
-$primary: #4a49cb;
-$danger: #fc4a4a;
-$warning: #fc9a1a;
-
-$btn-font-weight: 600;
-
-@import "../node_modules/bootstrap/scss/bootstrap";
-@import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
-</style>
+<style lang="scss"></style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,34 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <router-link class="navbar-brand" to="/assignments">
-        <img src="@/assets/brand.svg" height="38" width="38" />
-      </router-link>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarTop"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarTop">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/assignments"
-              >Assignments</router-link
-            >
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/boids">Boids</router-link>
-          </li>
-        </ul>
-        <button class="btn btn-primary">
-          <i class="bi bi-person-fill"></i>
-        </button>
-      </div>
-    </div>
-  </nav>
+  <nav></nav>
 </template>
 
 <script lang="ts">
